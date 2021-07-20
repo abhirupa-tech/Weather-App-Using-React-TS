1c71c386824fb1effa461ccd41be1c281b233660
Abhirupa Mitra <t-abmitra@microsoft.com>
2021-07-20 15:49:25 +0530

    first commit

---
+++ weather-app/src/App.tsx
@@ -0,0 +1,18 @@
+import React from 'react';
+// import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Weather App</h1>
+
+      <label>
+        Pick location:
+        <input type = "text" value = "Kolkata"></input>
+      </label>
+    </div>
+  );
+}
+
+export default App;
