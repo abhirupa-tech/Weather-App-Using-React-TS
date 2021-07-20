d714c22b965c9476abaa0acd2effc17b60e9b597
Abhirupa Mitra <t-abmitra@microsoft.com>
2021-07-20 17:12:08 +0530

    Adding location as states

--- weather-app/src/App.tsx
+++ weather-app/src/App.tsx
@@ -1,16 +1,45 @@
-import React from 'react';
+import React from "react";
 // import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+import "./App.css";
 
 function App() {
+  const [currentLocation, setLocationSearch] = useState("Kolkata");
+  const [locations, setLocation] = useState([
+    "Berlin",
+    "London",
+    "New York",
+    "Tokyo",
+  ]);
   return (
     <div className="App">
       <h1>Weather App</h1>
 
       <label>
         Pick location:
-        <input type = "text" value = "Kolkata"></input>
+        <input
+          type="text"
+          value={currentLocation}
+          onChange={(e) => setLocationSearch(e.target.value)}
+        ></input>
       </label>
+      <button>Search</button>
+
+      <table>
+        <thead>
+          <tr>
+            <th>Locations</th>
+          </tr>
+        </thead>
+        <tbody>
+          {locations.map((location, index) => (
+            <tr>
+              <td>{index}</td>
+              <td>{location}</td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
     </div>
   );
 }
