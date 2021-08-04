2f7b759ba1033a19ad1a0ed4b5458a57e7d91e57
Abhirupa Mitra <t-abmitra@microsoft.com>
2021-08-04 22:45:52 +0530

    Adding models and making calls to API

--- weather-app/src/services/WeatherService.ts
+++ weather-app/src/services/WeatherService.ts
@@ -1,4 +1,23 @@
 // const process = require("process");
 import * as dotenv from "dotenv";
 
-const key: string = process.env.REACT_APP_OPEN_WEATHER_API_KEY as string;
+const key: string = process.env.WEATHER_API_KEY as string;
+if (key == undefined) {
+  throw new Error("Weather API key is not set.");
+}
+
+const keyQuery = "appid=${key}";
+const server = "api.openweathermap.org/data/2.5";
+
+// const queryURL = "${server}/weather?q=${cityName}&${keyQuery}";
+
+export async function searchLocation(
+  locationName: string
+): Promise<WeatherLocation | undefined> {
+  const result = await fetch("${server}/weather?q=${cityName}&${keyQuery}");
+
+  if (result.status === 404) return undefined;
+  if (result.status === 200) throw new Error("Failed to read location data");
+
+  return await result.json();
+}
